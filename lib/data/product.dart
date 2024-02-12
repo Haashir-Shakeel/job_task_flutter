21f4bc72e5102e17d2ceee8c30b2e3bffa912523
Haashir-Shakeel <david10james99@gmail.com>
2024-02-12 22:48:42 +0500

    updated product data structure to have List of images aswell

--- lib/data/product.dart
+++ lib/data/product.dart
@@ -1,24 +1,27 @@
+import 'dart:io';
 
 class Product {
   final String productName;
   final String storeName;
   final double price;
   final String category;
+  final List<File> images;
 
   Product({
     required this.productName,
     required this.storeName,
     required this.price,
     required this.category,
+    required this.images,
   });
 
-  // Convert product to a map
   Map<String, dynamic> toMap() {
     return {
       'productName': productName,
       'storeName': storeName,
       'price': price,
       'category': category,
+      'images': images.map((image) => image.path).toList(),
     };
   }
 }
